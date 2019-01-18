3b3614268813ea29dbe90f49ee3ca99293a64300
Haoxin Gong <2330453961@qq.com>
2019-01-18 22:57:27 +0800

    fix 文件
    文件

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,20 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-    UserImage: '',
-    Username: '',
+    userImage:{},
+    nickname:{},
+    code:{}
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _this = this
-    wx.getUserInfo({
+    var that = this;
+    wx.login({
       success: function (res) {
-        _this.setData({
-          UserImage: res.userInfo.avatarUrl,
-          Username: res.userInfo.nickName
-        })
+        if (res.code) {
+          // 发起网络请求
+          wx.request({
+            url: 'https://test.com/onLogin',
+            data: {
+              code: res.code
+            },
+            method: 'POST',
+            header: {
+              'content-type': 'application/json'
+            },
+            successfunction(res) {
+              if (res.statusCode == 200) {
+                console.log("获取到的openid为：" + res.data);
+                that.userImage = res.data.userImage;
+                that.nickname = res.data.nickname;
+                wx.setStorageSync('openid', res.data);
+              } else {
+                console.log(res.errMsg);
+              }
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg);
+        }
       }
     })
   }
