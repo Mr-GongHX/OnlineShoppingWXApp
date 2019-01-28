639bc6175a55825f238c6b6e51fe87a81a187141
Haoxin Gong <2330453961@qq.com>
2019-01-29 00:08:54 +0800

    fix # bug

--- pages/orderDetail/orderDetail.js
+++ pages/orderDetail/orderDetail.js
@@ -5,18 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    orderId: "",
-    status: "",
-    name: "",
-    phone: "",
-    address: "",
-    shopName: "",
+    orderId: "1",
+    status: "0",
+    name: "张三",
+    phone: "184889541656",
+    address: "北京市海淀区完全户外活发货覅回复",
+    shopName: "苹果旗舰店",
+    amount: "2",
     goodsItem: [
       {
-        name: "",
-        iamge: "",
-        price: "",
-        amount: ""
+        name: "iPhoneXs Max",
+        image: "https://a4.vimage1.com/upload/flow/2017/10/20/117/15084947982974.jpg",
+        price: "6000",
+        amount: "1"
       }
     ]
 
