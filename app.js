5db3f62113cc94cc91ebbf3f04a40174081fef51
Haoxin Gong <2330453961@qq.com>
2019-01-31 15:12:31 +0800

    fix # 项目结构

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 //app.js
 App({
   onLaunch: function () {
- 
+    wx.login({
+      timeout: 10000,
+      success: (res) =>{
+        console.log(res.code);
+        wx.showToast({
+          title: '登录成功',
+          icon: 'success',
+          duration:1000
+        });
+      },
+      fail: () =>{
+        wx.showToast({
+          title: '登录失败',
+          icon: 'loading',
+          duration: 1000
+        });
+      }
+    });
   },
   globalData: {
     isLogin: true,
